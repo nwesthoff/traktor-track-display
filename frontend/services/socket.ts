48b0c15ef67ab6fe0a41b1b6c3960a479a84bd7a
Nils Westhoff <nils@nilswesthoff.com>
2021-08-05 00:56:28 +0200

    🐛 Use wss secure websocket

--- frontend/services/socket.ts
+++ frontend/services/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io(`ws://${process.env.NEXT_PUBLIC_SOCKET_ENDPOINT}`, {
+export const socket = io(`wss://${process.env.NEXT_PUBLIC_SOCKET_ENDPOINT}`, {
   reconnectionDelayMax: 10000,
 });
